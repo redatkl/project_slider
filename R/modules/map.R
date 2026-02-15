0bd3bdb65730decd41254d54bb5f73a45ed275ed
Reda TRANKIL <reda.trankil@gmail.com>
2026-02-15 12:29:28 +0000

    add map modulee

--- R/modules/map.R
+++ R/modules/map.R
@@ -10,56 +10,89 @@ mapUi <- function(id) {
       tags$script(src = "js/map_slider.js")
     ),
     tags$div(
-      id = ns("map_container"),
-      class = "map-slider-container",
-      `data-ns` = id,  # Pass namespace to JS
+      id = "map_container",
+      class = "map-container",
       
+      # Bottom map (full width)
       tags$div(
-        class = "map-left-container",
-        leafletOutput(ns("map_left"), width = "100%", height = "100%")
-      ),
+        class = "map-wrapper",
+        leafletOutput("map1", height = "600px")
+        ),
       
+      # Top map (clipped, full width)
       tags$div(
-        class = "map-right-container",
-        leafletOutput(ns("map_right"), width = "100%", height = "100%")
-      ),
+        class = "map-wrapper", 
+        id = "map2-wrapper", 
+        leafletOutput("map2", height = "600px")
+        ),
       
-      tags$div(class = "map-slider-divider")
+      tags$div(class = "slider-divider",
+               id = "slider-divider",
+               tags$div(class = "slider-handle")
+      )
     )
   )
 }
-
 
 # map module server
 mapServer <- function(id) {
   moduleServer(id, function(input, output, session) {
     
-    output$map_left <- renderLeaflet({
+    # Reactive value to store the slider position
+    slider_position <- reactiveVal(50)
+    
+    # Render the first map (OpenStreetMap)
+    output$map1 <- renderLeaflet({
       leaflet() %>%
-        setView(lng = -122.4194, lat = 37.7749, zoom = 12) %>%
-        addTiles() %>%
-        addMarkers(lng = -122.4194, lat = 37.7749, popup = "OpenStreetMap") %>%
-        htmlwidgets::onRender("
-          function(el, x) {
-            var map = this;
-            setTimeout(function() { map.invalidateSize(); }, 100);
-            window.mapLeft = map;
-          }
-        ")
+        addTiles(group = "OSM") %>%
+        setView(lng = -74.0060, lat = 40.7128, zoom = 12) %>%
+        addMarkers(lng = -74.0060, lat = 40.7128, popup = "New York - Street Map")
     })
     
-    output$map_right <- renderLeaflet({
+    # Render the second map (Satellite imagery)
+    output$map2 <- renderLeaflet({
       leaflet() %>%
-        setView(lng = -122.4194, lat = 37.7749, zoom = 12) %>%
-        addProviderTiles(providers$Esri.WorldImagery) %>%
-        addMarkers(lng = -122.4194, lat = 37.7749, popup = "Satellite") %>%
-        htmlwidgets::onRender("
-          function(el, x) {
-            var map = this;
-            setTimeout(function() { map.invalidateSize(); }, 100);
-            window.mapRight = map;
-          }
-        ")
+        addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+        setView(lng = -74.0060, lat = 40.7128, zoom = 12) %>%
+        addMarkers(lng = -74.0060, lat = 40.7128, popup = "New York - Satellite View")
     })
-  })
-}
+    
+    # Sync map movements from map1 to map2
+    observeEvent(input$map1_zoom, {
+      if (!is.null(input$map1_zoom) && !is.null(input$map1_center)) {
+        leafletProxy("map2") %>%
+          setView(
+            lng = input$map1_center$lng,
+            lat = input$map1_center$lat,
+            zoom = input$map1_zoom
+          )
+      }
+    })
+    
+    # Sync map movements from map2 to map1
+    observeEvent(input$map2_zoom, {
+      if (!is.null(input$map2_zoom) && !is.null(input$map2_center)) {
+        leafletProxy("map1") %>%
+          setView(
+            lng = input$map2_center$lng,
+            lat = input$map2_center$lat,
+            zoom = input$map2_zoom
+          )
+      }
+    })
+    
+    # Handle slider position updates from JavaScript
+    observeEvent(input$slider_pos, {
+      slider_position(input$slider_pos)
+    })
+    
+    # Initialize the slider functionality
+    observe({
+      session$sendCustomMessage(
+        type = "initSlider",
+        message = list()
+      )
+    })
+  }
+  )}
+  
--- www/js/map_slider.js
+++ www/js/map_slider.js
@@ -1,39 +1,38 @@
-$(document).on('shiny:idle', function() {
-  setTimeout(function() {
-    $('.map-slider-container').each(function() {
-      var container = $(this);
-      var ns = container.data('ns');
-      var leftContainer = container.find('.map-left-container');
-      var rightContainer = container.find('.map-right-container');
-      var slider = container.find('.map-slider-divider');
-      var isDragging = false;
-      
-      slider.on('mousedown touchstart', function(e) {
-        isDragging = true;
-        e.preventDefault();
-      });
-      
-      $(document).on('mousemove touchmove', function(e) {
-        if (isDragging) {
-          var clientX = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
-          var containerOffset = container.offset().left;
-          var containerWidth = container.width();
-          var mouseX = clientX - containerOffset;
-          var percentage = (mouseX / containerWidth) * 100;
-          percentage = Math.max(10, Math.min(90, percentage));
-          
-          leftContainer.css('width', percentage + '%');
-          rightContainer.css('width', (100 - percentage) + '%');
-          slider.css('left', percentage + '%');
-          
-          if (window.mapLeft) window.mapLeft.invalidateSize();
-          if (window.mapRight) window.mapRight.invalidateSize();
-        }
-      });
-      
-      $(document).on('mouseup touchend', function() {
-        isDragging = false;
-      });
-    });
-  }, 500);
+Shiny.addCustomMessageHandler('initSlider', function(message) {
+  var container = document.getElementById('map-container');
+  var divider = document.getElementById('slider-divider');
+  var map2Wrapper = document.getElementById('map2-wrapper');
+  var isDragging = false;
+  
+  function updatePosition(x) {
+    var rect = container.getBoundingClientRect();
+    var position = ((x - rect.left) / rect.width) * 100;
+    position = Math.max(0, Math.min(100, position));
+    
+    divider.style.left = position + '%';
+    map2Wrapper.style.clipPath = 'inset(0 ' + (100 - position) + '% 0 0)';
+    
+    Shiny.setInputValue('slider_pos', position);
+  }
+  
+  divider.addEventListener('mousedown', function(e) {
+    isDragging = true;
+    e.preventDefault();
+  });
+  
+  document.addEventListener('mousemove', function(e) {
+    if (isDragging) {
+updatePosition(e.clientX);
+    }
+  });
+  
+  document.addEventListener('mouseup', function() {
+    isDragging = false;
+  });
+  
+  container.addEventListener('click', function(e) {
+    if (!isDragging && e.target === container) {
+updatePosition(e.clientX);
+    }
+  });
 });
